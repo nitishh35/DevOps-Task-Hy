3ff75ffafa47c992cc0da8edfd8692a170526310
nitishh35 <nitishh35@gmail.com>
2025-05-29 14:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
+import logging
 
+# Create Flask app
 app = Flask(__name__)
 
-# Enable metrics
+# Set up Prometheus metrics endpoint (/metrics)
 metrics = PrometheusMetrics(app)
+
+# logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def hello():
+    logger.info("Home route accessed")
     return "Hello from Flask!"
 
 @app.route('/health')
 def health():
+    logger.info("Health check route accessed")
     return "OK", 200
 
 if __name__ == '__main__':
